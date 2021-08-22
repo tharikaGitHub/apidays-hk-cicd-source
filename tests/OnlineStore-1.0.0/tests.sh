7ace5543eff61a475dc72650b793291f81286446
Tharika Madurapperuma <tharika.m92@gmail.com>
2021-08-22 15:57:49 +0530

    Add tests for online store

--- tests/OnlineStore-1.0.0/tests.sh
+++ tests/OnlineStore-1.0.0/tests.sh
@@ -7,5 +7,9 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 # retrieve a token to invoke and test the API
 token=$(apictl get keys -n OnlineStore -v 1.0.0 -e dev -k)
 
+echo $DIR
+
+echo $token
+
 # run newman test script
 newman run $DIR/OnlineStore.postman_collection.json --env-var="token=$token" -k
